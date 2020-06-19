013f9c0305465aeb22d175383ab7a70c3d1801dd
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-06-19 07:55:03 -0700

    backend auth is working

---
+++ controllers/auth.js
@@ -0,0 +1,88 @@
+const bcrypt = require('bcrypt')
+
+const register = (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({
+            status: 400,
+            message: "Please enter a name, email, and password"
+        })
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return res.status(500).json({
+            status: 500,
+            message: 'Something went wrong. Please try again.'
+        })
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+            if (err) return res.status(500).json({
+                status: 500,
+                message: 'Something went wrong. Please try again.'
+            })
+
+            var userData = {
+                "first_name": req.body.first_name,
+                "last_name": req.body.last_name,
+                "email": req.body.email,
+                "password": hash,
+                "created": new Date()
+               }
+
+
+            db.query('INSERT INTO users SET ?',userData, (err, result) => {
+                
+                if (err) return res.status(500).json({ status: 500, message: err })
+                return res.status(200).json({ status: 200, message: "User registered!",result })
+            });
+        })         
+    })
+
+}
+
+const login = (req,res) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({ status: 400, message: 'Please enter your email and password' });
+    }
+
+    let email = req.body.email;
+    let password = req.body.password;
+    
+    db.query("SELECT * FROM users WHERE email = ?",[email],(err,results)=>{
+        if (err) return res.status(500).json({ status: 500, message: 'Something went wrong. Please try again' });
+        if(results.length==0) return res.status(500).json({ status: 500, message: 'No User with this email' });
+
+        // res.json(results[0].password)
+        bcrypt.compare(req.body.password, results[0].password, (err, isMatch) => {
+            if (err) return res.status(500).json({ status: 500, message: 'Something went wrong. Please try again' });
+
+            if (isMatch) {
+                req.session.currentUser = { id: results[0].id };
+                return res.status(200).json({ status: 200, message: 'Success', data: results[0].id });
+            } else {
+                return res.status(400).json({ status: 400, message: 'Email or password is incorrect' });
+            }
+        });
+        
+
+    })
+}
+
+const logout = (req, res) => {
+    if (!req.session.currentUser) return res.status(401).json({ status: 401, message: 'Unauthorized' });
+    req.session.destroy((err) => {
+        if (err) return res.status(500).json({ status: 500, message: 'Something went wrong. Please try again' });
+        res.sendStatus(200);
+    });
+};
+const verify = (req, res) => {
+    if (!req.session.currentUser) return res.status(401).json({ status: 401, message: 'Unauthorized' });
+    res.status(200).json({
+        status: 200,
+        message: `Current User verified. User ID: ${req.session.currentUser.id}`
+    });
+};
+
+module.exports = {
+    register,
+    login,
+    logout,
+    verify
+}
--- server.js
+++ server.js
@@ -1,36 +1,76 @@
 const express = require('express');
 const mysql = require('mysql')
+const cors = require('cors')
+const morgan = require('morgan')
+require('dotenv').config()
+const routes = require('./routes')
+const session = require('express-session');
+const app = express();
+const bodyParser=require("body-parser");
+const MySQLStore = require('express-mysql-session')(session);
+const PORT = process.env.PORT || 3030;
 
-const con = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'a1b2c3D4.',
-    database :  top250songs
-});
+const corsOptions = {
+    origin: [`http://localhost:3000`],
+    credentials: true, // allows the session cookie to be sent back and forth from server to client
+    optionsSuccessStatus: 200 // some legacy browsers choke on status 204
+}
+
+var options = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'a1b2c3D4.',
+    database: 'top250songs',
+};
+ 
+
+
+const con = mysql.createConnection(options);
+
+var sessionStore = new MySQLStore(options);
+
+app.use(session({
+    key: 'top250',
+    secret: 'verysecret',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 // cookie will expire in 1 week
+    }
+}));
+
+
 
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Table created");
-    });
 });
 
+global.db = con;
 
+app.use(cors(corsOptions))
 
-const app = express()
+            // Logging with Morgan
+app.use(morgan('tiny'))
   
-// app.get('/createdb', (req,res)=> {
-//     let sql = "CREATE DATABASE top250songs";
-//     db.query(sql, (err,result)=>{
-//         if(err) throw err;
-//         console.log(result)
-//         res.send("database created")
-//     })
-// }) 
 
-app.listen('3000',()=> {
-    console.log('Server started on port 3000')
+app.set('port', process.env.PORT || 8080);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+//Routes
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Testing</h1>')
+})
+
+app.use('/api/v1/auth', routes.auth)
+
+app.listen(PORT,()=> {
+    console.log(`Server is running at localhost:${PORT}`)
 })
