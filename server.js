0f060ef6361cf02cf10b7e1cf3126949c866f606
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-07-04 19:19:15 -0700

    debug for cors policy heroku

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.use(bodyParser.json());
 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://www.top250songs.com/");
+    res.header("Access-Control-Allow-Origin", "https://www.top250songs.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
