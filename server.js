ec685c9bb30cfad6e07aa91ebc37521f227b689b
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-07-04 19:00:21 -0700

    still

--- server.js
+++ server.js
@@ -60,11 +60,11 @@ app.use(bodyParser.json());
 
 
 //Routes
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
   
 
 app.get('/', (req, res) => {
