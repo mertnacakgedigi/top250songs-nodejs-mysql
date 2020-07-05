cdcd754e1f614f57cc2517959c69bc7e507dc454
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-07-04 18:54:55 -0700

    cors edited

--- server.js
+++ server.js
@@ -60,7 +60,12 @@ app.use(bodyParser.json());
 
 
 //Routes
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+  
 
 app.get('/', (req, res) => {
     res.send('<h1>Testing</h1>')
