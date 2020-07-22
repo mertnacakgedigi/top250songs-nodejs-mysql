357bba57e9353b04d8a77ff71fe397c214f30788
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-07-22 15:22:51 -0700

    enough

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const corsOptions = {
 }
 // app.use(cors({origin : "http://localhost:3000"}))
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
