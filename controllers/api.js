554d57c4526bf7a006e7376453acc1c2c8baa1e5
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-07-22 15:17:36 -0700

    enough

--- controllers/api.js
+++ controllers/api.js
@@ -1,5 +1,8 @@
 const getList = (req,res) => {
-
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
     var sql="SELECT songs.id,songs.name,songs.artist,songs.image,COUNT(ratings.rating) as c,ratings.rating as r, AVG(ratings.rating) as a FROM songs LEFT JOIN ratings ON ratings.song_id=songs.id GROUP BY songs.name ORDER BY a desc"
     db.query(sql,(err,result)=>{
         if (err) return res.status(500).json({ status: 500, message: err })
@@ -8,7 +11,10 @@ const getList = (req,res) => {
 }
 
 const getRating = (req,res) => {
-
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
     var sql="SELECT * FROM ratings"
     db.query(sql,(err,result)=>{
         if (err) return res.status(500).json({ status: 500, message: err })
@@ -17,6 +23,10 @@ const getRating = (req,res) => {
 }
 
 const postRating = (req,res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
     var sql = "INSERT INTO ratings SET ?";
     db.query(sql,req.body,(err,result) => {
         if (err) return res.status(500).json({ status: 500, message: err })
@@ -25,6 +35,10 @@ const postRating = (req,res) => {
 }
 
 const getUserRating = (req,res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
    var sql = `SELECT songs.id as song_id,songs.name,songs.artist,COUNT(ratings.rating) as c,ratings.rating as r, AVG(ratings.rating) as a, ratings.user_id FROM songs LEFT JOIN ratings ON ratings.song_id=songs.id Right Join ratings as ra on ratings.user_id =${req.body.user_id} GROUP BY songs.name ORDER BY a desc`
     db.query(sql,(err,result) => {
         if (err) return res.status(500).json({ status: 500, message: err })
