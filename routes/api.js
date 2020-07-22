563ac0edc85d70690d749b28376d54271abfa868
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-07-22 15:55:24 -0700

    enough

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const ctrl = require('../controllers')
 
 router.get('/list',ctrl.api.getList)
 router.get('/rating',ctrl.api.getRating)
-router.post('/rating',ctrl.api.postRating)
+router.post('/ratinguser',ctrl.api.postRating)
 router.post('/userrating',ctrl.api.getUserRating)
 
 module.exports = router
