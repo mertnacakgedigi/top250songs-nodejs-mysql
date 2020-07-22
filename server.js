12981632c3707c85ba6088e781f91a51b5df9be5
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-07-21 22:54:10 -0700

    heroku debug

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use(bodyParser.json());
 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://www.top250songs.com/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
