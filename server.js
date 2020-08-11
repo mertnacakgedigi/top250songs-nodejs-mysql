50a5a4fdfa8831cc6d789c3a32368e586193cf2c
mertnacakgedigi <51537954+mertnacakgedigi@users.noreply.github.com>
2020-08-11 11:30:01 -0700

    Update server.js

--- server.js
+++ server.js
@@ -33,8 +33,6 @@ var options = {
     password: 'nqPNE9N36r',
     database: 'sql3356294',
 };
- 
-
 
 const con = mysql.createConnection(options);
 
@@ -51,40 +49,14 @@ app.use(session({
     }
 }));
 
-
-
 con.connect();
 
 global.db = con;
-
-// app.use(function (req, res, next) {
-
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-
-//     // Pass to next layer of middleware
-//     next();
-// });
- 
-
 
 
 app.set('port', process.env.PORT || 8080);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-
-
 
 app.get('/', (req, res) => {
     res.send('<h1>Testing</h1>')
